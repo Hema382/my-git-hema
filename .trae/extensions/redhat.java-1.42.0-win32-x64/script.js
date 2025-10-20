f17b7b77e757ddc6ec3630918a3f310b15ffc195
Hema382 <hemachowdary0405@gmail.com>
2025-10-21 00:15:02 +0530

    Delete .trae/extensions/redhat.java-1.42.0-win32-x64 directory

--- .trae/extensions/redhat.java-1.42.0-win32-x64/script.js
+++
@@ -1,7 +0,0 @@
-function showMessage() {
-    alert("Hello! This is a message from the external JavaScript file!");
-}
-
-function showMessage() {
-    alert("Hello! This is a message from the external JavaScript file!");
-}
